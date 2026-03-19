5483efe3baeb56bee66504b663cfaf26e8a9df6b
taneputaranui <putaranuitane@gmail.com>
2026-03-19 17:45:33 +1300

    Skeleton Command Line Interface.

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ import pandas as pd
 import numpy as np
 
 def main():
-    print("=== Country Data Analysis Tool ===")
-
-    df= None
-    clean_df = None
-
+    print("=== World Data Analysis Tool ===")
     while True:
+
+ 
         print("\nMenu:")
+        print("1. Load and clean data")
+        print("2. Answer analysis questions")
+        print("3. Exit")
+
+        choice = input("Select an option: ")
+
+        # 3. Exit
+        if choice == "3":
+            print("Exiting program.")
+            break
+        else:
+            print("Invalid option. Try again.")
 
 if __name__ == "__main__":
     main()
